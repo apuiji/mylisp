1306ff37594b82563d2d3e8091b5290733594d2a
apuiji <2330683684@qq.com>
2024-03-29 18:44:21 +0800

    a

---
+++ ast_nodes1.hh
@@ -0,0 +1,148 @@
+#pragma once
+
+#include<array>
+#include<set>
+#include"ast.hh"
+#include"ast_token.hh"
+
+namespace zlt::mylisp::ast {
+  struct Call final: Node {
+    UNode callee;
+    UNodes args;
+    Call(const char *start, UNode &&callee, UNodes &&args) noexcept:
+    Node(start), callee(std::move(callee)), args(std::move(args)) {}
+  };
+
+  struct Callee final: Node {
+    using Node::Node;
+  };
+
+  struct Defer final: Node {
+    UNode item;
+    Defer(const char *start, UNode &&item) noexcept: Node(start), item(std::move(item)) {}
+  };
+
+  struct Forward final: Node {
+    UNode callee;
+    UNodes args;
+    Forward(const char *start, UNode &&callee, UNodes &&args) noexcept:
+    Node(start), callee(std::move(callee)), args(std::move(args)) {}
+  };
+
+  struct Function final: Node {
+    using Defs = std::set<const std::string *>;
+    using Params = std::vector<const std::string *>;
+    Defs defs;
+    Params params;
+    UNodes body;
+    Function(const char *start, Defs &&defs, Params &&params, UNodes &&body) noexcept:
+    Node(start), defs(std::move(defs)), params(std::move(params)), body(std::move(body)) {}
+  };
+
+  struct ID final: Node {
+    const std::string *name;
+    ID(const char *start, const std::string *name) noexcept: Node(start), name(name) {}
+  };
+
+  struct If final: Node {
+    UNode cond;
+    UNode then;
+    UNode elze;
+    If(const char *start, UNode &&cond, UNode &&then, UNode &&elze) noexcept:
+    Node(start), cond(std::move(cond)), then(std::move(then)), elze(std::move(elze)) {}
+  };
+
+  struct Null final: Node {
+    using Node::Node;
+  };
+
+  struct Number final: Node {
+    double value;
+    Number(const char *start, double value) noexcept: Node(start), value(value) {}
+  };
+
+  struct Return final: Node {
+    UNode value;
+    Return(const char *start, UNode &&value) noexcept: Node(start), value(std::move(value)) {}
+  };
+
+  struct Throw final: Node {
+    UNode value;
+    Throw(const char *start, UNode &&value) noexcept: Node(start), value(std::move(value)) {}
+  };
+
+  struct Try final: Node {
+    UNodes body;
+    Try(const char *start, UNodes &&body) noexcept: Node(start), body(std::move(body)) {}
+  };
+
+  struct Yield final: Node {
+    UNode then;
+    Yield(const char *start, UNode &&then) noexcept: Node(start), then(std::move(then)) {}
+  };
+
+  // operations begin
+  template<int N>
+  struct Operation: Node {
+    std::array<UNode, N> items;
+    Operation(const char *start, std::array<UNode, N> &&items) noexcept: Node(start), items(std::move(items)) {}
+  };
+
+  template<>
+  struct Operation<1>: Node {
+    UNode item;
+    Operation(const char *start, UNode &&item) noexcept: Node(start), item(std::move(item)) {}
+  };
+
+  template<>
+  struct Operation<-1>: Node {
+    UNodes items;
+    Operation(const char *start, UNodes &&items) noexcept: Node(start), items(std::move(items)) {}
+  };
+
+  template<int N, int Op>
+  struct Operation1 final: Operation<N> {
+    using Operation<N>::Operation;
+  };
+
+  // arithmetical operations begin
+  using ArithAddOper = Operation1<-1, "+"_token>;
+  using ArithSubOper = Operation1<-1, "-"_token>;
+  using ArithMulOper = Operation1<-1, "*"_token>;
+  using ArithDivOper = Operation1<-1, "/"_token>;
+  using ArithModOper = Operation1<-1, "%"_token>;
+  using ArithPowOper = Operation1<-1, "**"_token>;
+  // arithmetical operations end
+  // logical operations begin
+  using LogicAndOper = Operation1<-1, "&&"_token>;
+  using LogicOrOper = Operation1<-1, "||"_token>;
+  using LogicNotOper = Operation1<1, "!"_token>;
+  using LogicXorOper = Operation1<-1, "^^"_token>;
+  // logical operations end
+  // bitwise operations begin
+  using BitwsAndOper = Operation1<-1, "&"_token>;
+  using BitwsOrOper = Operation1<-1, "|"_token>;
+  using BitwsNotOper = Operation1<1, "~"_token>;
+  using BitwsXorOper = Operation1<-1, "^"_token>;
+  using LshOper = Operation1<-1, "<<"_token>;
+  using RshOper = Operation1<-1, ">>"_token>;
+  using UshOper = Operation1<-1, ">>>"_token>;
+  // bitwise operations end
+  // compare operations begin
+  using CmpEqOper = Operation1<2, "=="_token>;
+  using CmpLtOper = Operation1<2, "<"_token>;
+  using CmpGtOper = Operation1<2, ">"_token>;
+  using CmpLteqOper = Operation1<2, "<="_token>;
+  using CmpGteqOper = Operation1<2, ">="_token>;
+  using CompareOper = Operation1<2, "<=>"_token>;
+  // compare operations end
+  using AssignOper = Operation1<2, "="_token>;
+  using GetMemberOper = Operation1<-1, "."_token>;
+  using LengthOper = Operation1<1, "length"_token>;
+  using SequenceOper = Operation1<-1, ","_token>;
+
+  struct SetMemberOper final: Operation<3> {
+    using Operation<3>::Operation;
+  };
+  // operations end
+}
---
+++ ast_token.cc
@@ -0,0 +1,108 @@
+#include<regex>
+#include"ast_token.hh"
+
+using namespace std;
+
+namespace zlt::mylisp::ast::token {
+  static bool isNumber(double &dest, const char *start, std::string_view raw);
+
+  int ofRaw(double &numval, const char *start, std::string_view raw) {
+    if (isNumber(numval, start, raw)) {
+      return NUMBER;
+    }
+    #define ifSymbol(symb) \
+    if (raw == symb) { \
+      return symb##_token; \
+    }
+    #define ifKeyword(kwd) ifSymbol(#kwd)
+    ifKeyword(callee);
+    ifKeyword(def);
+    ifKeyword(defer);
+    ifKeyword(forward);
+    ifKeyword(if);
+    ifKeyword(length);
+    ifKeyword(return);
+    ifKeyword(throw);
+    ifKeyword(try);
+    ifKeyword(yield);
+    #undef ifKeyword
+    ifSymbol("!");
+    ifSymbol("#");
+    ifSymbol("##");
+    ifSymbol("#def");
+    ifSymbol("#def");
+    ifSymbol("#ifdef");
+    ifSymbol("#ifndef");
+    ifSymbol("#include");
+    ifSymbol("#undef");
+    ifSymbol("%");
+    ifSymbol("&&");
+    ifSymbol("&");
+    ifSymbol("**");
+    ifSymbol("*");
+    ifSymbol("+");
+    ifSymbol(",");
+    ifSymbol("-");
+    ifSymbol(".");
+    ifSymbol("/");
+    ifSymbol("<<");
+    ifSymbol("<=>");
+    ifSymbol("<=");
+    ifSymbol("<");
+    ifSymbol("==");
+    ifSymbol("=");
+    ifSymbol(">=");
+    ifSymbol(">>>");
+    ifSymbol(">>");
+    ifSymbol(">");
+    ifSymbol("@");
+    ifSymbol("^^");
+    ifSymbol("^");
+    ifSymbol("||");
+    ifSymbol("|");
+    ifSymbol("~");
+    #undef ifSymbol
+    return ID;
+  }
+
+  static bool isBaseInt(double &dest, string_view src);
+
+  bool isNumber(double &dest, const char *start, string_view src) {
+    try {
+      if (isBaseInt(dest, src)) {
+        return true;
+      }
+      try {
+        dest = stod(string(src));
+        return true;
+      } catch (invalid_argument) {
+        return false;
+      }
+    } catch (out_of_range) {
+      throw AstBad(bad::NUMBER_LITERAL_OOR, start);
+    }
+  }
+
+  static bool isBaseInt(double &dest, const regex &re, size_t base, string_view src);
+
+  bool isBaseInt(double &dest, string_view src) {
+    static const regex re2("([+-]?)0[Bb]([01]+)");
+    static const regex re4("([+-]?)0[Qq]([0-3]+)");
+    static const regex re8("([+-]?)0[Oo]([0-7]+)");
+    static const regex re16("([+-]?)0[Xx]([[:xdigit:]]+)");
+    return
+      isBaseInt(dest, re2, 2, src) ||
+      isBaseInt(dest, re4, 4, src) ||
+      isBaseInt(dest, re8, 8, src) ||
+      isBaseInt(dest, re16, 16, src);
+  }
+
+  bool isBaseInt(double &dest, const regex &re, size_t base, string_view src) {
+    match_results<string_view::const_iterator> m;
+    if (!regex_match(src.begin(), src.end(), m, re)) {
+      return false;
+    }
+    dest = stoi(m.str(1) + m.str(2), nullptr, base);
+    return true;
+  }
+}
